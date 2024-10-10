40ed98c1e8656c578d31d1c67b77fb6151c6d116
SaimonNeupane <Saimonbro00007@gmail.com>
2024-10-10 11:22:13 +0545

    optimized font

--- kaushal/src/app/page.jsx
+++ kaushal/src/app/page.jsx
@@ -1,33 +1,48 @@
-import React from 'react'
-import "../styles/global.css"
-const page = () => {
+import React from 'react';
+import "../styles/global.css";
+
+const Page = () => {
   return (
-    <main className="px-4 sm:px-8 md:px-16 lg:px-24 xl:px-[400px]">
-      <div className=" flex flex-col-reverse lg:flex-row  gap-[4px]  ">
-        <div className="px:0  text-center lg:text-left" >
+    <main style={{ paddingLeft: '10%',
+      paddingRight:'10%',
+      paddingTop:'3%'
+     }}>
+      <div className="flex flex-col-reverse lg:flex-row gap-4">
+        {/* Text Container */}
+        <div className="w-full lg:w-1/2 text-center lg:text-left">
           <span className="Hollow">C</span>
           <span className="Hollow">A</span>
           <span className="Hollow">R</span>
           <span className="Hollow">I</span>
           <span className="Hollow">N</span>
           <span className="Hollow">G</span><br />
-
-          <span className="Hollow " style={{
+          <span className="Hollow" style={{
             WebkitTextStroke: '2px black',
             color: 'transparent',
-          }}
-          >CLIMATE</span><br />
+          }}>
+            CLIMATE
+          </span><br />
           <span className="smallHollow">Curing PLANET</span>
+        </div>
 
 
+        <div className="flex justify-center lg:justify-end w-full lg:w-1/2">
+          <img 
+            src="/images/Logo.png" 
+            className="py-10 w-full ] h-auto mx-auto lg:mx-0" 
+            alt="Logo" 
+            style={{ display: 'block' }} 
+          />
         </div>
-        <div className="justify-center text-center" >
-          <img src="/images/Logo.png" className="py-10  w-full h-auto" alt="" />
-        </div>
-        <div></div>
       </div>
-    </main >
-  )
+      <div className="text-center lg:text-left w-full lg:w-1/2" >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit aspernatur soluta sit debitis, minima ex, at quos velit, commodi ratione molestiae in deserunt quis aliquid maxime est architecto. Voluptatibus, ipsam!
+      </div>
+      <div className="text-center py-[50px] lg:py-[25px] font-bold question"  >
+        <span>WHAT DOES CCN DO?</span>
+      </div>
+    </main>
+  );
 }
 
-export default page
+export default Page;
