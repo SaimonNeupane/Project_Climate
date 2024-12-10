2a4e3f5082bdfc80eeab35bfcbed9a581333c22b
SaimonNeupane <Saimonbro00007@gmail.com>
2024-12-10 22:35:07 +0545

    modified next.js

--- kaushal/next.config.mjs
+++ kaushal/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/Kaushal', // Adjust this to match the subfolder where your app is hosted
+  };
+  
+  export default nextConfig;
+  
