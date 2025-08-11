456946698b19cb3ba180d9101248f89eda34952b
SaimonNeupane <Saimonbro00007@gmail.com>
2025-08-12 02:42:03 +0545

    final Backend

--- server/routes/EmailRoute.js
+++ server/routes/EmailRoute.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.post("/verify", EmailValidator);
 router.post("/send", sendEmailForVolunteer);
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Email route is working" });
+});
 
 export default router;
